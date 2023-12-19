0ecacc98d2527f98289435cb0199954ff0d9a245
tijsziere <39266480+tijsziere@users.noreply.github.com>
2023-12-19 12:10:45 +0100

    fix error

--- main.py
+++ main.py
@@ -218,8 +218,7 @@ async def kobo_to_121(request: Request, dependencies=Depends(required_headers_12
     """Send a Kobo submission to 121."""
 
     kobo_data = await request.json()
-    kobo_data = clean_kobo_data(kobo_data)
-    if kobo_data['skipConnect']:
+    if 'skipConnect' in kobo_data and kobo_data['skipConnect']:
         quit()
     
     attachments = get_attachment_dict(kobo_data)
