807ab1371ec22ed42c89f0814cb3a0aded06d190
jmargutti <jacopo.margutti@gmail.com>
2024-04-03 12:27:14 +0200

    increase timeout attachments

--- main.py
+++ main.py
@@ -131,9 +131,11 @@ def get_attachment_dict(kobo_data, kobotoken=None, koboasset=None):
         data = data_request.json()
         if '_attachments' in data.keys():
             attachments_list = data['_attachments']
+    logging.info(f"Attachment list 0: {attachments_list}")
     if len(attachments_list) == 0:
         if '_attachments' in kobo_data.keys():
             attachments_list = kobo_data['_attachments']
+    logging.info(f"Attachment list 1: {attachments_list}")
     for attachment in attachments_list:
         filename = attachment['filename'].split('/')[-1]
         downloadurl = attachment['download_large_url']
