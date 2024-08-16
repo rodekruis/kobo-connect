43c26f7c3cb26fa6b198f19843246a06aae702bf
Jacopo Margutti <jacopo.margutti@gmail.com>
2024-08-16 09:27:01 +0200

    change espo error logging

--- main.py
+++ main.py
@@ -171,7 +171,8 @@ def espo_request(submission, espo_client, method, entity, params=None, logs=None
         response = espo_client.request(method, entity, params)
         return response
     except HTTPException as e:
-        logger.error(f"EspoCRM: {e.detail}", extra=logs)
+        detail = e.detail if "Unknown Error" not in e.detail else ""
+        logger.error(f"Failed: EspoCRM returned {e.status_code} {detail}", extra=logs)
         update_submission_status(submission, "failed", e.detail)
 
 
