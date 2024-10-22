ab942b6622b2191706a747d074ea2c4e9a7b1b04
tijsziere <39266480+tijsziere@users.noreply.github.com>
2024-10-22 18:40:23 +0200

    fix main

--- main.py
+++ main.py
@@ -4,33 +4,12 @@ from fastapi.responses import RedirectResponse, JSONResponse
 import requests
 import os
 from dotenv import load_dotenv
+from utils.logger import logger
 from routes import routes121, routesEspo, routesGeneric, routesKobo
 
 # load environment variables
 load_dotenv()
 port = os.environ["PORT"]
-
-# Set up logs export to Azure Application Insights
-logger_provider = LoggerProvider()
-set_logger_provider(logger_provider)
-exporter = AzureMonitorLogExporter(
-    connection_string=os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
-)
-logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
-
-# Attach LoggingHandler to root logger
-handler = LoggingHandler()
-logging.getLogger().addHandler(handler)
-logging.getLogger().setLevel(logging.NOTSET)
-logger = logging.getLogger(__name__)
-
-# Silence noisy loggers
-logging.getLogger("requests").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
-logging.getLogger("azure").setLevel(logging.WARNING)
-logging.getLogger("requests_oauthlib").setLevel(logging.WARNING)
-logging.getLogger("asyncio").setLevel(logging.WARNING)
-logging.getLogger("opentelemetry").setLevel(logging.ERROR)
 
 # initialize FastAPI
 app = FastAPI(
@@ -45,16 +24,6 @@ app = FastAPI(
         "url": "https://www.gnu.org/licenses/agpl-3.0.en.html",
     },
 )
-
-# initialize CosmosDB
-client_ = cosmos_client.CosmosClient(
-    os.getenv("COSMOS_URL"),
-    {"masterKey": os.getenv("COSMOS_KEY")},
-    user_agent="kobo-connect",
-    user_agent_overwrite=True,
-)
-cosmos_db = client_.get_database_client("kobo-connect")
-cosmos_container_client = cosmos_db.get_container_client("kobo-submissions")
 
 
 @app.get("/", include_in_schema=False)
